2194cbabbc35bf83635fbe1a2cba62c0e08c183a
woktvm <woktvm@gmail.com>
2025-09-09 18:25:45 +0300

    Add files via upload

---
+++ LaboratorPC_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a,b,c,x,F;
+	printf("Introdu variabilele pentru operatie a,b,c si x: \n");
+	scanf("%f %f %f %f", &a, &b, &c, &x);
+	if(x<0 && b !=0){
+	    F =  a-cos(x)/(10+b);
+        printf("Rezultatul este: %f\n", F);
+	} else if(x>0 || b == 0){
+	    F = (x-a)/(x-b);
+	    printf("Rezultatul este: %f\n", F);
+	} else {
+	    F = 3*pow(x,2) + a/c;
+	    printf("Rezultatul este: %f\n", F);
+	}
+ 
+
+	return 0;
+}
